0cb941022796b866d7ce0ba4f95cba3204abcefc
“seenu4selenium” <seenu4selenium@gmail.com>
2024-12-23 19:21:44 -0700

    Prgms 23dec2024

---
+++ src/test/java/testscenarios/Assignment_6.java
@@ -0,0 +1,30 @@
+package testscenarios;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment_6 {
+
+	public static void main(String[] args) throws Exception {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+		//wait for few seconds
+		//Thread.sleep(9000);
+		
+		//implicit wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		
+		
+
+
+	}
+
+}
--- src/test/java/testscenarios/FB_login.java
+++ src/test/java/testscenarios/FB_login.java
@@ -1,5 +1,7 @@
 package testscenarios;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -18,11 +20,30 @@ public class FB_login {
 		driver.get("https://www.facebook.com/");
 
 		// Type UN & Pwd
-		driver.findElement(By.name("email")).sendKeys("Vivek");
+		driver.findElement(By.name("email")).sendKeys("rajithashwetha1234@gmail.com");
 		driver.findElement(By.name("pass")).sendKeys("DGshsdfhsfhfs");
 
 		// click on login button
 		driver.findElement(By.name("login")).click();
+		//Wait for page load
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+//		//find the error message has displayed on webpage "Find your account and log in."	
+//		if (driver.findElement(By.linkText("Find your account and log in.abc")).isDisplayed()) {
+//			 String eleText =  driver.findElement(By.linkText("Find your account and log in.")).getText();
+//			 System.out.println(eleText);
+//		} else {
+//			System.out.println("The locator is not disaplyed.");
+//		}
+		
+		
+		if (driver.findElements(By.linkText("Find your account and log in.")).size()>0) {
+			 String eleText =  driver.findElement(By.linkText("Find your account and log in.")).getText();
+			 System.out.println(eleText);
+		} else {
+			System.out.println("The locator is not disaplyed.");
+		}
+
 
 	}
 
